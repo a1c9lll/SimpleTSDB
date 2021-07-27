4b89705031a49dc2996676541ed0becce1fcb293
a1c9lll <litty123x@protonmail.com>
2021-07-26 21:54:00 -0400

    add pg ping to test connection

--- datastore/connection.go
+++ datastore/connection.go
@@ -19,6 +19,9 @@ func InitDB(pgUser, pgPassword, pgHost string, pgPort int, pgDB, pgSSLMode strin
 	if err != nil {
 		log.Fatal(err)
 	}
+	if err := session.Ping(); err != nil {
+		log.Fatal(err)
+	}
 	session.Query(fmt.Sprintf("create database %s", pgDB))
 	if err := session.Close(); err != nil {
 		log.Fatal(err)
@@ -26,6 +29,9 @@ func InitDB(pgUser, pgPassword, pgHost string, pgPort int, pgDB, pgSSLMode strin
 	connStr := fmt.Sprintf("user=%s password='%s' host='%s' port=%d dbname=%s sslmode=%s", pgUser, pgPassword, pgHost, pgPort, pgDB, pgSSLMode)
 	session, err = sql.Open("postgres", connStr)
 	if err != nil {
+		log.Fatal(err)
+	}
+	if err := session.Ping(); err != nil {
 		log.Fatal(err)
 	}
 }
